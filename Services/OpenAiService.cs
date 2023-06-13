cf6c1bf72034a2ebd05fb34253166bacf7b63d60
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2023-06-13 21:12:37 +0200

    explaining code for Ai Service

--- Services/OpenAiService.cs
+++ Services/OpenAiService.cs
@@ -9,39 +9,43 @@ namespace DotNETwithGPT.Services
     {
         private readonly OpenAiConfig _config;
 
-        public OpenAiService(IOptionsMonitor<OpenAiConfig> optionsMonitor) 
+        public OpenAiService(IOptionsMonitor<OpenAiConfig> optionsMonitor)
         {
             _config = optionsMonitor.CurrentValue;
         }
-        
+
+        // Method to check if the given input is a programming language or not
         public async Task<string> CheckProgramingLanguage(string language)
         {
             var api = new OpenAI_API.OpenAIAPI(_config.Key);
 
             var chat = api.Chat.CreateConversation();
-            // configuration of answers 
-            chat.AppendSystemMessage("You are a teacher who help new programmers understand things are programming language or not. If the user tells you a programming language respond with yes, if a user tells you something which is not a programming language respond with no. you will only respond with yes or no. you do not say anything else.");
+            // Configure the conversation with system message and user input
+            chat.AppendSystemMessage("You are a teacher who helps new programmers understand what things are programming languages or not. If the user tells you a programming language, respond with 'yes'. If the user tells you something which is not a programming language, respond with 'no'. You will only respond with 'yes' or 'no'; do not say anything else.");
 
             chat.AppendUserInput(language);
 
+            // Get the response from the chatbot
             var response = await chat.GetResponseFromChatbotAsync();
 
             return response;
         }
 
+        // Method to complete a sentence using the OpenAI GPT model
         public async Task<string> CompleteSentence(string text)
         {
-            // api instance with simple answers like input one two and answer three ...
+            // Create an API instance for simple text completion
             var api = new OpenAI_API.OpenAIAPI(_config.Key);
             var result = await api.Completions.GetCompletion(text);
             return result;
         }
 
+        // Method to advanced sentence completion using the OpenAI GPT model
         public async Task<string> CompleteSentenceAdvanced(string text)
         {
-            // api instance with model where temperature means how broadly will text model answer where small values mean keeping close to topic and 1.0 means that model have the most freedom in answer
+            // Create an API instance with a model that provides more freedom in the answer
             var api = new OpenAI_API.OpenAIAPI(_config.Key);
-            var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(text, model:Model.CurieText, temperature: 0.1));
+            var result = await api.Completions.CreateCompletionAsync(new CompletionRequest(text, model: Model.CurieText, temperature: 0.1));
             return result.Completions[0].Text;
         }
     }
